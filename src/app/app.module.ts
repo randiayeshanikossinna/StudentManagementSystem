62cb43cc7fd430630f44c17999da160e3c3b17cc
randiayeshanikossinna <randiayeshani@gmail.com>
2021-03-12 08:42:40 -0800

    Navigation bar was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/Toolbar';
+import { MatIconModule } from '@angular/material/Icon';
+import { MatSidenavModule } from '@angular/material/Sidenav';
+import { MatListModule } from '@angular/material/List';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule ,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule ,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
